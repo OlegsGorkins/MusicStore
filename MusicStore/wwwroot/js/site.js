8c06991956d4afca217d7347d27a13f12fbfd390
Olegs <olegs.gorkins@gmail.com>
2018-03-09 23:46:00 +0200

    Add application description and contact information.

--- MusicStore/wwwroot/js/site.js
+++ MusicStore/wwwroot/js/site.js
@@ -6,3 +6,18 @@ function GoBack() {
 function ChangeID(id, newID) {
     document.getElementById(id).id = newID;
 }
+
+function CopyToClipboard(id) {
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(id));
+        range.select().createTextRange();
+        document.execCommand("copy");
+
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(id));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+    }
+}
