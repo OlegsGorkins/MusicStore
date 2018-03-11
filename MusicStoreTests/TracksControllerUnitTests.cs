69520e27ef9885ffe230039a19c085de6fc68a5d
Olegs <olegs.gorkins@gmail.com>
2018-03-11 20:35:23 +0200

    Add TracksController.Details return value test.

---
+++ MusicStoreTests/TracksControllerUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Models;
+using MusicStore.Controllers;
+using MusicStore.Data;
+using Xunit;
+using FluentAssertions;
+
+namespace MusicStoreTests
+{
+    public class TracksControllerUnitTests
+    {
+        [Fact]
+        public async void Details_Values()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                //Arrange
+                var options = new DbContextOptionsBuilder<MusicStoreContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                var context = new MusicStoreContext(options);
+                context.Database.EnsureCreated();
+                SeedData.SeedDatabase(context);
+
+                var controller = new TracksController(context);
+
+                int id = 1;
+
+                //Act
+                var result = await controller.Details(id);
+
+                //Assert
+                var view = result.Should().BeOfType<ViewResult>().Subject;
+                var track = view.Model.Should().BeAssignableTo<Track>().Subject;
+                track.ID.Should().Be(id);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
